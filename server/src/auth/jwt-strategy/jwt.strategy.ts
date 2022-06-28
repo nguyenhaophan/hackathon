ab1dd0b1f2af028012b751b8d9cf9373590b6fb7
Hao Phan <nguyenhao.phan@gmail.com>
2022-06-28 13:12:52 +0300

    reset again

--- server/src/auth/jwt-strategy/jwt.strategy.ts
+++ server/src/auth/jwt-strategy/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { PassportStrategy } from '@nestjs/passport'
 import { ExtractJwt, Strategy } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
 
 import { UsersService } from 'src/users/users.service'
 
